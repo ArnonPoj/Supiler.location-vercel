0146296ee0c087df68302f73e6b5bd921351f9b7
ArnonPoj <bensen.f@hotmail.com>
2026-01-17 14:06:24 +0700

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,8 +1,1 @@
-    c.execute('''
-        CREATE TABLE IF NOT EXISTS transport_pickup_personal (
-            id SERIAL PRIMARY KEY,
-            driver_name TEXT NOT NULL,
-            phone TEXT,
-            note TEXT
-        )
-    ''')
+
